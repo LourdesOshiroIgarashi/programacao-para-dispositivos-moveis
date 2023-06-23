e7af5caacdd7c61b20fedb2fefb8a7e77a7ede25
Everton de Oliveira <everton.oliveira@ufms.br>
2023-06-23 18:51:11 -0400

    feat(aluno): adicionado a função que salva o aluno no Firebase

--- app/src/main/java/com/example/labin/view/entities/Aluno.java
+++ app/src/main/java/com/example/labin/view/entities/Aluno.java
@@ -1,2 +1,62 @@
-package com.example.labin.view.entities;public class Aluno {
+package com.example.labin.view.entities;
+
+
+public class Aluno {
+
+    private String nome;
+    private String email;
+    private String laboratorio;
+    private String curso;
+    private String telefone;
+
+
+
+    public Aluno(String nome, String email, String laboratorio, String curso) {
+        this.nome = nome;
+        this.email = email;
+        this.laboratorio = laboratorio;
+        this.curso = curso;
+    }
+    public Aluno() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLaboratorio() {
+        return laboratorio;
+    }
+
+    public void setLaboratorio(String laboratorio) {
+        this.laboratorio = laboratorio;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
--- app/src/main/java/com/example/labin/view/view/AdicionarAluno.java
+++ app/src/main/java/com/example/labin/view/view/AdicionarAluno.java
@@ -1,19 +1,31 @@
 package com.example.labin.view.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.labin.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.example.labin.view.entities.Aluno;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AdicionarAluno extends AppCompatActivity {
 
     private EditText nomeAluno, laboratorioAluno, emailAluno, cursoAluno;
     private Button btnSalvarAluno;
 
-    
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +40,38 @@ public class AdicionarAluno extends AppCompatActivity {
 
 
 
+
+        /*aluno1.setNome("Everton");
+        aluno1.setCurso("Engenharia de Software");
+        aluno1.setEmail("everton.oliveira@ufms.br");
+        aluno1.setLaboratorio("batlab");*/
+
+        /*aluno1.setNome("Lourdes");
+        aluno1.setCurso("Engenharia de Software");
+        aluno1.setEmail("lourdes.oshiro@ufms.br");
+        aluno1.setLaboratorio("lia");*/
+    }
+
+    public void SalvarDados(View view){
+
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome(nomeAluno.getText().toString());
+        aluno1.setCurso(cursoAluno.getText().toString());
+        aluno1.setEmail(emailAluno.getText().toString());
+        aluno1.setLaboratorio(laboratorioAluno.getText().toString());
+
+
+
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Aluno");
+
+        myRef.child("3").setValue(aluno1);
+
+
+        Toast.makeText(getApplicationContext(), "Dados salvos", Toast.LENGTH_LONG).show();
+        finish();
     }
 }
