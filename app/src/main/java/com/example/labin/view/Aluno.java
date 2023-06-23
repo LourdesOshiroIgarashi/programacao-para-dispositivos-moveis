d66a98c3741a87ea4b4f4a7541e4a052923ef85c
Everton de Oliveira <everton.oliveira@ufms.br>
2023-06-23 16:38:34 -0400

    feat(aluno): adicionando recursos de navegação para as atividades de adicionar aluno e visualizar aluno

--- app/src/main/java/com/example/labin/view/Aluno.java
+++ app/src/main/java/com/example/labin/view/Aluno.java
@@ -2,15 +2,36 @@ package com.example.labin.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.labin.R;
 
 public class Aluno extends AppCompatActivity {
 
+    private Button addAluno, viewAluno;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aluno);
+
+        //conectando os botões com o código Java
+        addAluno = findViewById(R.id.addAluno);
+        viewAluno = findViewById(R.id.viewAluno);
+
+
+    }
+
+
+    public void adicionarAluno(View view){
+        Intent intent = new Intent(getApplicationContext(), AdicionarAluno.class);
+        startActivity(intent);
+    }
+
+    public void visualizarAluno(View view){
+        Intent intent = new Intent(getApplicationContext(), VisualizarAluno.class);
+        startActivity(intent);
     }
 }
