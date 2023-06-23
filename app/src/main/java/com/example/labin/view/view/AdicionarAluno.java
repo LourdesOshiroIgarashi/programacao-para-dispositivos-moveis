43852a40a155c5123235e62da5e4a5d641cff122
Everton de Oliveira <everton.oliveira@ufms.br>
2023-06-23 17:38:29 -0400

    feat(aluno): adicionando recursos de conexão dos componentes com o código java

--- app/src/main/java/com/example/labin/view/view/AdicionarAluno.java
+++ app/src/main/java/com/example/labin/view/view/AdicionarAluno.java
@@ -3,14 +3,28 @@ package com.example.labin.view.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.labin.R;
 
 public class AdicionarAluno extends AppCompatActivity {
 
+    private EditText nomeAluno, laboratorioAluno, emailAluno, cursoAluno;
+    private Button btnSalvarAluno;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_aluno);
+
+        nomeAluno  = findViewById(R.id.nomeAluno);
+        laboratorioAluno = findViewById(R.id.laboratorioAluno);
+        emailAluno = findViewById(R.id.emailAluno);
+        cursoAluno = findViewById(R.id.cursoAluno);
+        btnSalvarAluno = findViewById(R.id.btnSalvarAluno);
+        
+
+
     }
 }
