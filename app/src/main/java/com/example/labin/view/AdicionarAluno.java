c130e96fe78379c60b34199e01d4f0e233ea41fd
Everton de Oliveira <everton.oliveira@ufms.br>
2023-06-23 16:29:02 -0400

    adicionando novas atividades e layouts no projeto

---
+++ app/src/main/java/com/example/labin/view/AdicionarAluno.java
@@ -0,0 +1,16 @@
+package com.example.labin.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.labin.R;
+
+public class AdicionarAluno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_aluno);
+    }
+}
---
+++ app/src/main/java/com/example/labin/view/VisualizarLaboratorio.java
@@ -0,0 +1,16 @@
+package com.example.labin.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.labin.R;
+
+public class VisualizarLaboratorio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualizar_laboratorio);
+    }
+}
