8dcda787345d9b2111584a10c17a376b212b7925
Everton de Oliveira <everton.oliveira@ufms.br>
2023-06-23 23:49:03 -0400

    feat(fragmento): adicionado o recurso de alterar um fragmento quando clicado em um botão

--- app/src/main/java/com/example/labin/view/fragment/AlunosFragment.java
+++ app/src/main/java/com/example/labin/view/fragment/AlunosFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.labin.R;
 
@@ -25,6 +26,7 @@ public class AlunosFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private TextView textAlunos;
 
     public AlunosFragment() {
         // Required empty public constructor
@@ -61,6 +63,8 @@ public class AlunosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alunos, container, false);
+        View view =  inflater.inflate(R.layout.fragment_alunos, container, false);
+        textAlunos = view.findViewById(R.id.textAlunos);
+        return view;
     }
 }
--- app/src/main/java/com/example/labin/view/view/VisualizarLaboratorio.java
+++ app/src/main/java/com/example/labin/view/view/VisualizarLaboratorio.java
@@ -1,18 +1,54 @@
 package com.example.labin.view.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.labin.R;
+import com.example.labin.view.fragment.AlunosFragment;
+import com.example.labin.view.fragment.LaboratoriosFragment;
 
 public class VisualizarLaboratorio extends AppCompatActivity {
 
+
+    private Button btnLab, btnAluno;
+    private AlunosFragment alunosFragment;
+    private LaboratoriosFragment laboratoriosFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visualizar_laboratorio);
 
+        btnLab = findViewById(R.id.btnLab);
+        btnAluno = findViewById(R.id.btnAluno);
 
+
+
+
+
+
+
+        btnAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alunosFragment = new AlunosFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frameConteudo, alunosFragment);
+                transaction.commit();
+            }
+        });
+
+        btnLab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                laboratoriosFragment = new LaboratoriosFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frameConteudo, laboratoriosFragment);
+                transaction.commit();
+            }
+        });
     }
 }
