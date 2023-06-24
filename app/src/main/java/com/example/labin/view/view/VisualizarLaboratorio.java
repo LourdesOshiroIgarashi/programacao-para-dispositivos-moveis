9a18f19249bfbbdde46d033f8ed965470af6ed69
Everton de Oliveira <everton.oliveira@ufms.br>
2023-06-23 23:35:35 -0400

    feat(fragmentos): adicionado os fragmentos labs e alunos

--- app/src/main/java/com/example/labin/view/view/VisualizarLaboratorio.java
+++ app/src/main/java/com/example/labin/view/view/VisualizarLaboratorio.java
@@ -13,6 +13,6 @@ public class VisualizarLaboratorio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visualizar_laboratorio);
 
-        
+
     }
 }
